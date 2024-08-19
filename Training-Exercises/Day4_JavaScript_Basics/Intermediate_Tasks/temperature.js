9c3cbdae64beea0f6eda3ba73daba3363f9c450a
Anusha-K-Satheesh <158844649+Anusha-K-Satheesh@users.noreply.github.com>
2024-08-19 11:56:24 +0530

    Delete Training-Exercises/Day4_JavaScript_Basics/Intermediate_Tasks/temperature.js

--- Training-Exercises/Day4_JavaScript_Basics/Intermediate_Tasks/temperature.js
+++
@@ -1,7 +0,0 @@
-function toFahrenheit(event) {
-    event.preventDefault();
-    var celsius = document.getElementsByName("cel")[0].value
-    var fahrenheit = (celsius * 9/5) + 32
-    document.getElementsByName("fah")[0].value = fahrenheit
-    return false
-}
